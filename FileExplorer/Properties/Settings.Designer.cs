3f6b3377647bfd8302fd4db4cc35021d9f6db3cc
omery <omeryanar@gmail.com>
2021-06-14 22:14:45 +0300

    Default Width & Height values.

--- FileExplorer/Properties/Settings.Designer.cs
+++ FileExplorer/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FileExplorer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     public sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -325,7 +325,7 @@ namespace FileExplorer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1280")]
         public double WindowWidth {
             get {
                 return ((double)(this["WindowWidth"]));
@@ -337,7 +337,7 @@ namespace FileExplorer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("720")]
         public double WindowHeight {
             get {
                 return ((double)(this["WindowHeight"]));
